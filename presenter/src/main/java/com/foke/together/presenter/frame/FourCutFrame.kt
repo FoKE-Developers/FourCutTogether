29b002b7f4d968a40e4d78474f9fe4c1ff0fa3d8
김윤수 <ing03201@gmail.com>
2024-09-28 22:40:01 +0900

    add custom CutFrame 2 types (FourcutFrame, MakerFaireFrame) [#32] FourCutFrame - can add colorScheme to change Frame color

---
+++ presenter/src/main/java/com/foke/together/presenter/frame/FourCutFrame.kt
@@ -0,0 +1,105 @@
+package com.foke.together.presenter.frame
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import com.foke.together.presenter.R
+import com.foke.together.presenter.ui.theme.FourCutTogetherTheme
+import com.foke.together.presenter.ui.theme.highContrastDarkColorScheme
+import com.foke.together.presenter.ui.theme.mediumContrastLightColorScheme
+
+val frameRatio = 0.3333f
+@Composable
+fun FourCutFrame(
+    designColorScheme: ColorScheme = mediumContrastLightColorScheme,
+    cameraImageUrlList : List<String>? = null,
+) {
+    ConstraintLayout(
+        modifier = Modifier
+            .aspectRatio(ratio = 0.3333f)
+            .background(color = designColorScheme.surface)
+            .padding(10.dp)
+    ) {
+        val (startBarrier, endBarrier, cameraColumn, background, decorateImage, curTime) = createRefs()
+        LazyColumn(
+            state = rememberLazyListState(),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            contentPadding = PaddingValues(horizontal = 10.dp, vertical = 10.dp),
+            modifier = Modifier
+                .constrainAs(cameraColumn) {
+                    top.linkTo(parent.top, margin = 15.dp)
+                    bottom.linkTo(decorateImage.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    width = Dimension.fillToConstraints
+                    height = Dimension.wrapContent
+                }
+
+        ) {
+            items(4){
+                Box(
+                    modifier = Modifier
+                        .aspectRatio(1.5f)
+                        .background(color = designColorScheme.inverseSurface)
+                )
+            }
+        }
+
+        Icon(
+            imageVector =  ImageVector.vectorResource(
+                id = R.drawable.fourcut_together
+            ),
+            contentDescription = "for decorate",
+            modifier = Modifier
+                .constrainAs(decorateImage) {
+                    top.linkTo(cameraColumn.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .size(
+                    width = 48.dp,
+                    height = 48.dp
+                ),
+            tint = designColorScheme.primaryContainer
+        )
+        Text(
+            text = "2024.09.27",
+            modifier = Modifier.constrainAs(curTime){
+                top.linkTo(decorateImage.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
+            color = designColorScheme.inverseSurface,
+            fontSize = 12.sp
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DefaultFrame() {
+    FourCutTogetherTheme {
+        FourCutFrame(
+            designColorScheme = highContrastDarkColorScheme
+        )
+    }
+}
---
+++ presenter/src/main/java/com/foke/together/presenter/frame/MakerFaireFrame.kt
@@ -0,0 +1,108 @@
+package com.foke.together.presenter.frame
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import com.foke.together.presenter.R
+import com.foke.together.presenter.ui.theme.FourCutTogetherTheme
+
+@Composable
+fun MakerFaireFrame(
+    cameraImageUrlList : List<String>? = null,
+    backgroundColor : Color = Color(0xFFF5B934),
+    decorateImageUrl: String? = null,
+) {
+    ConstraintLayout(
+        modifier = Modifier
+            .aspectRatio(ratio = 0.3333f)
+            .background(backgroundColor)
+            .padding(10.dp)
+    ) {
+        val (startBarrier, endBarrier, cameraColumn, background, decorateImage, curTime) = createRefs()
+        LazyColumn(
+            state = rememberLazyListState(),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            contentPadding = PaddingValues(horizontal = 10.dp, vertical = 10.dp),
+            modifier = Modifier
+                .constrainAs(cameraColumn) {
+                    top.linkTo(parent.top, margin = 15.dp)
+                    bottom.linkTo(decorateImage.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .wrapContentSize()
+        ) {
+            items(4){
+                if(backgroundColor == Color.White) {
+                    Box(
+                        modifier = Modifier
+                            .aspectRatio(1.5f)
+                            .background(color = Color.Black)
+                    )
+                }
+                else{
+                    Box(
+                        modifier = Modifier
+                            .aspectRatio(1.5f)
+                            .background(color = Color.White)
+                    )
+                }
+            }
+        }
+        Image(
+            painter =  painterResource(
+                id = R.drawable.maker_faire_logo
+            ),
+            contentDescription = "for decorate",
+            modifier = Modifier.constrainAs(decorateImage){
+                top.linkTo(cameraColumn.bottom)
+                bottom.linkTo(curTime.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                height = Dimension.wrapContent
+                width = Dimension.fillToConstraints
+            }
+        )
+        Text(
+            text = "2024.09.27",
+            modifier = Modifier.constrainAs(curTime){
+                top.linkTo(decorateImage.bottom)
+                bottom.linkTo(parent.bottom, margin = 15.dp)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
+            color = Color.White,
+            fontSize = 12.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DefaultFrame() {
+    FourCutTogetherTheme {
+        MakerFaireFrame()
+    }
+}
