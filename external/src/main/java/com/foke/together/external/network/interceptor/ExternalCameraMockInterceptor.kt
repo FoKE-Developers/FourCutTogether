11bcaff86b34458a2391e88143e94f0f62e1eaa0
Tony Dokyoon Kim <uhug@naver.com>
2024-10-09 15:52:37 +0900

    change mock interceptor name [#35]
    - 빌드 에러 해결

--- external/src/main/java/com/foke/together/external/network/interceptor/ExternalCameraMockInterceptor.kt
+++ external/src/main/java/com/foke/together/external/network/interceptor/ExternalCameraMockInterceptor.kt
@@ -4,7 +4,7 @@ import okhttp3.Interceptor
 import okhttp3.Protocol
 import okhttp3.Response
 
-class MockInterceptor: Interceptor {
+class ExternalCameraMockInterceptor: Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         // TODO: make mock for test
         return Response.Builder()
