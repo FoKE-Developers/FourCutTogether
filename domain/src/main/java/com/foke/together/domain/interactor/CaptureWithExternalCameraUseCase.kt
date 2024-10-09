3f2c620964d56fa5a66e8864452e7e3adf41088a
Tony Dokyoon Kim <uhug@naver.com>
2024-10-09 18:09:27 +0900

    add file save code in CaptureWithExternalCameraUseCase [#58]

--- domain/src/main/java/com/foke/together/domain/interactor/CaptureWithExternalCameraUseCase.kt
+++ domain/src/main/java/com/foke/together/domain/interactor/CaptureWithExternalCameraUseCase.kt
@@ -1,20 +1,25 @@
 package com.foke.together.domain.interactor
 
+import android.content.Context
 import com.foke.together.domain.output.ExternalCameraRepositoryInterface
 import com.foke.together.util.AppLog
+import com.foke.together.util.ImageFileUtil
+import com.foke.together.util.TimeUtil
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
+// TODO: CameraScreen에서  filepath를 TimeUtil.getCurrentTimeSec() 로 전달
+//  filepath를 ScameraScreen -> GenerateScreen -> ShareScreen 로 전달
+//  추후에는 filepath를 presenter에서 관리하지 않도록 해야 할듯
 class CaptureWithExternalCameraUseCase @Inject constructor(
+    @ApplicationContext private val context: Context,
     private val externalCameraRepository: ExternalCameraRepositoryInterface
 ) {
-    suspend operator fun invoke(): Result<Unit> {
+    suspend operator fun invoke(filepath: String): Result<Unit> {
         externalCameraRepository.capture()
             .onSuccess {
-                // TODO: save Bitmap to internal storage
-                AppLog.i(TAG, "capture", "success: $it")
-
-                // TODO: implement file save code here ...
-
+                // TODO: create file module in phase4
+                ImageFileUtil.saveBitmap(context, it, filepath, TimeUtil.getCurrentTimeMillis())
                 return Result.success(Unit)
             }
             .onFailure {
