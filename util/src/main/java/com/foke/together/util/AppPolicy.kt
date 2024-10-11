0e43198c647c5d7023652dc221691d5a704c06f5
Tony Dokyoon Kim <uhug@naver.com>
2024-10-12 05:33:17 +0900

    do some chores [#85]

--- util/src/main/java/com/foke/together/util/AppPolicy.kt
+++ util/src/main/java/com/foke/together/util/AppPolicy.kt
@@ -3,8 +3,6 @@ package com.foke.together.util
 object AppPolicy {
     const val isDebugMode = true
 
-    const val DEFAULT_EXTERNAL_CAMERA_IP = "192.168.0.71:5000"
-
     // network
     const val WEB_SERVER_URL = "https://4cuts.store/"
     const val WEB_CONNECT_TIMEOUT = 10L
@@ -12,18 +10,19 @@ object AppPolicy {
     const val WEB_WRITE_TIMEOUT = 10L
     const val WEB_FILE_MAX_CONTENT_LENGTH = 20971520
 
-    const val EXTERNAL_CAMERA_DEFAULT_SERVER_URL = "http://192.168.0.71:5000"
+    const val EXTERNAL_CAMERA_DEFAULT_SERVER_URL = "http://0.0.0.0"
     const val EXTERNAL_CAMERA_CONNECT_TIMEOUT = 10L
     const val EXTERNAL_CAMERA_READ_TIMEOUT = 10L
     const val EXTERNAL_CAMERA_WRITE_TIMEOUT = 10L
 
+    // capture settings
     const val CAPTURE_INTERVAL = 10000L
     const val CAPTURE_COUNT = 4
     const val COUNTDOWN_INTERVAL = 10L
 
+    // file settings
     const val CAPTURED_FOUR_CUT_IMAGE_NAME = "capture"
     const val SINGLE_ROW_FINAL_IMAGE_NAME = "final_single_row"
     const val TWO_ROW_FINAL_IMAGE_NAME = "final_two_row"
-
     const val DEFAULT_QR_CODE_IMAGE_NAME = "qrcode"
 }
