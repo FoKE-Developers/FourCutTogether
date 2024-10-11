2d4ed99ae258364e49064969c471e0e2ffeaf0cf
Tony Dokyoon Kim <uhug@naver.com>
2024-10-12 03:48:54 +0900

    fix home screen design [#27]

--- presenter/src/main/java/com/foke/together/presenter/screen/HomeScreen.kt
+++ presenter/src/main/java/com/foke/together/presenter/screen/HomeScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,12 +39,12 @@ fun HomeScreen(
                     id = R.drawable.fourcut_together
                 ),
                 contentDescription = "FourCuts Together",
+
                 modifier = Modifier.constrainAs(play) {
                     centerTo(parent)
                     height = Dimension.wrapContent
                     width = Dimension.wrapContent
                 },
-
             )
 
             IconButton(
@@ -76,7 +77,7 @@ fun HomeScreen(
                     }
             ) {
                 Text(
-                    text = "시작하기",
+                    text = stringResource(id = R.string.home_button_start),
                     style = MaterialTheme.typography.displayLarge,
                     fontWeight = FontWeight.Bold,
                     fontSize = 36.sp
