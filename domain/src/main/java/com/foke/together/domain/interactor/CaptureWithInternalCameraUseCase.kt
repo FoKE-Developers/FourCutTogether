FoKE-Developers/4-Cuts-Together

feat: implement internal camera functionality with capture and preview use cases [#45]

domain/src/main/java/com/foke/together/domain/interactor/CaptureWithInternalCameraUseCase.kt
@@ -1,0 +1,34 @@
+package com.foke.together.domain.interactor
+
+import android.content.Context
+import com.foke.together.domain.output.ImageRepositoryInterface
+import com.foke.together.domain.output.InternalCameraRepositoryInterface
+import com.foke.together.util.AppLog
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class CaptureWithInternalCameraUseCase @Inject constructor(
+    private val internalCameraRepository: InternalCameraRepositoryInterface,
+    private val imageRepository: ImageRepositoryInterface
+) {
+    suspend operator fun invoke(
+        context: Context,
+        fileName: String
+    ): Result<Unit>{
+        internalCameraRepository.capture(context)
+            .onSuccess {
+                AppLog.i(TAG, "capture", "success: $it")
+                imageRepository.cachingImage(it, fileName)
+                return Result.success(Unit)
+            }
+            .onFailure {
+                AppLog.i(TAG, "capture", "failure: $it")
+                return Result.failure(it)
+            }
+        return Result.failure(Exception("Unknown error"))
+    }
+
+    companion object {
+        private val TAG = CaptureWithInternalCameraUseCase::class.java.simpleName
+    }
+}

FoKE-Developers/FourCutTogether

domain/src/main/java/com/foke/together/domain/interactor/CaptureWithInternalCameraUseCase.kt
@@ -1,0 +1,34 @@
+package com.foke.together.domain.interactor
+
+import android.content.Context
+import com.foke.together.domain.output.ImageRepositoryInterface
+import com.foke.together.domain.output.InternalCameraRepositoryInterface
+import com.foke.together.util.AppLog
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class CaptureWithInternalCameraUseCase @Inject constructor(
+    private val internalCameraRepository: InternalCameraRepositoryInterface,
+    private val imageRepository: ImageRepositoryInterface
+) {
+    suspend operator fun invoke(
+        context: Context,
+        fileName: String
+    ): Result<Unit>{
+        internalCameraRepository.capture(context)
+            .onSuccess {
+                AppLog.i(TAG, "invoke", "success: $it")
+                imageRepository.cachingImage(it, fileName)
+                return Result.success(Unit)
+            }
+            .onFailure {
+                AppLog.e(TAG, "invoke", "failure: $it")
+                return Result.failure(it)
+            }
+        return Result.failure(Exception("Unknown error"))
+    }
+
+    companion object {
+        private val TAG = CaptureWithInternalCameraUseCase::class.java.simpleName
+    }
+}