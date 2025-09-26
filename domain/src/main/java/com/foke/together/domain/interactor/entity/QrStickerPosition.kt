8c7c661826624e49bfc3bbebc7846e235389c323
Tony Dokyoon Kim <uhug@naver.com>
2025-09-27 00:13:26 +0900

    modify CutFrameType entity for add more cutframes [#155]

---
+++ domain/src/main/java/com/foke/together/domain/interactor/entity/QrStickerPosition.kt
@@ -0,0 +1,7 @@
+package com.foke.together.domain.interactor.entity
+
+data class QrStickerPosition (
+    val size: Int,
+    val x: Int,
+    val y: Int,
+)
--- presenter/src/main/java/com/foke/together/presenter/frame/DefaultCutFrame.kt
+++ presenter/src/main/java/com/foke/together/presenter/frame/DefaultCutFrame.kt
@@ -1,7 +1,9 @@
 package com.foke.together.presenter.frame
 
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -12,22 +14,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.foke.together.domain.R
 import com.foke.together.domain.interactor.entity.DefaultCutFrameSet
 import com.foke.together.util.TimeUtil
 
 @Composable
 fun DefaultCutFrame(
     cutFrame: DefaultCutFrameSet,
-    imageUrlList : List<Uri>
+    imageUrlList : List<Uri>,
+    qrImageBitmap: Bitmap? = null
 ) {
     Box(
         modifier = Modifier
@@ -74,12 +81,24 @@ fun DefaultCutFrame(
             )
         }
 
-        // !!!!! TODO. date string
+        // Copyright Text Sticker
+        Text(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(cutFrame.copyrightPosition.backgroundColor)
+                .offset(x = cutFrame.copyrightPosition.x.dp, y = cutFrame.copyrightPosition.y.dp),
+            text = stringResource(com.foke.together.presenter.R.string.copyright),
+            color = cutFrame.copyrightPosition.color,
+            fontSize = cutFrame.copyrightPosition.fontSize.sp,
+            fontWeight = cutFrame.copyrightPosition.fontWeight
+        )
+
+        // Date Text Sticker
         if (cutFrame.isDateString) {
             Text(
                 modifier = Modifier
                     .fillMaxSize()
-                    .offset(y = cutFrame.dateStringHeight.dp),
+                    .offset(x = cutFrame.datePosition.x.dp, y = cutFrame.datePosition.y.dp),
                 text = TimeUtil.getCurrentDisplayTime(),
                 color = Color.White,
                 fontSize = 9.sp,
@@ -88,6 +107,24 @@ fun DefaultCutFrame(
             )
         }
 
-        // TODO: QRCode image
+        // QR Code Sticker
+        if (cutFrame.isQrCode) {
+            val qrBitmap = qrImageBitmap ?: ImageBitmap.imageResource(id = R.drawable.qr_sample)
+            Box(
+                modifier = Modifier
+                    .width(cutFrame.qrCodePosition.size.dp)
+                    .height(cutFrame.qrCodePosition.size.dp)
+                    .offset(x = cutFrame.qrCodePosition.x.dp, y = cutFrame.qrCodePosition.y.dp),
+            ) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(qrBitmap)
+                        .build(),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
     }
 }
